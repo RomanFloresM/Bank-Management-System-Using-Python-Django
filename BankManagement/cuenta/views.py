3f91c31c32bcfe8e6e62e252f654b7eb299d486b
RomanFloresM <cuatro.roman@gmail.com>
2025-02-08 01:11:25 -0600

    funcionamiento de login

--- BankManagement/cuenta/views.py
+++ BankManagement/cuenta/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
-
-def login(request, user, passw):
-    if user == "admin" and passw == "1234":
-        return render(request, 'cuenta/dashboard.html')
+    if request.method == 'POST':
+        user = request.POST['usuario']
+        passw = request.POST['contrasena']
+        validar = login(user=user, passw=passw)
+        if validar:
+            return redirect('menu')
+        else:
+            return render(request, 'index.html', {"cuenta": user, "validar": validar})
     else:
-        return HttpResponse("Usuario o contraseña incorrectos")
+        return render(request, 'index.html', {"validar": True})
+
+def login(user, passw):
+    if user == "admin" and passw == "1234":
+        return True
+    else:
+        return False
     
 def menu(request):
     return render(request, 'menu.html')
